d50864a9300eed766b1bff3c639db290550e5a84
kterhar-edu <81818592+kterhar-edu@users.noreply.github.com>
2024-04-05 15:17:22 +0000

    added a new column: released on

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -55,7 +55,7 @@ class MoviesController < ApplicationController
   private
 
   def movie_params
-      params.require(:movie).permit(:title, :description, :image_url)
+      params.require(:movie).permit(:title, :description, :image_url, :released_on)
   end
 
 end
---
+++ db/migrate/20240405151102_add_release_on_to_movies.rb
@@ -0,0 +1,5 @@
+class AddReleaseOnToMovies < ActiveRecord::Migration[7.0]
+  def change
+    add_column :movies, :released_on, :date
+  end
+end
